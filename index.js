cd7835aa2abb7fb981e2de87fe06006a252d3ffc
Kelley Shackelford <kelleyalexis@yahoo.com>
2023-10-17 00:46:18 -0400

    looping with for and while

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(cards, occasion) {
+  let messages = [];
+  for (let i = 0; i < cards.length; i++) {
+    messages.push(`Thank you, ${cards[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return messages;
+}
+
+function countDown() {
+  let countdown = 10;
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");
+countDown();
